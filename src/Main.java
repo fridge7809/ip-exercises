c42f062ad28dd424294e5af1fd58cc2c4e438fce
fridge7809 <christiannielsen@Christians-MBP.home>
2023-08-26 12:51:18 +0200

    Added template exercise class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TestExercise testExercise = new TestExercise("Add two numbers");
+        testExercise.addTwoNumbers(3, 6);
+        testExercise.printResult();
     }
 }
