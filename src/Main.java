213735d1bde3b396a23cf60d2741eb4359895a84
fridge7809 <christiannielsen@Christians-MacBook-Pro.local>
2023-08-28 14:11:32 +0200

    Handin1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        TestExercise testExercise = new TestExercise("Add two numbers");
-        testExercise.addTwoNumbers(3, 6);
-        testExercise.printResult();
+        System.out.println("Hello, World!");
     }
 }
